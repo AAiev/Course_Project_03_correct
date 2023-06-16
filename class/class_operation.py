07ee8d95e449b5b617765c3c4af49623b69eb672
Aleksandr Ievlev <a.a.ievlev@gmail.com>
2023-06-16 22:20:24 +0300

    add date_operation for class Operation in class_operation.py

--- class/class_operation.py
+++ class/class_operation.py
@@ -4,3 +4,13 @@ class Operation:
 
     def __repr__(self):
         return f"Operation(dict_operation={self.dict_operation})"
+
+    def date_operation(self):
+        """
+        возвращает дату в формате: ДД.ММ.ГГГГ
+        :return: дата в формате ДД.ММ.ГГГГ
+        """
+        date_operation_year = self.dict_operation['date'][0:4]
+        date_operation_month = self.dict_operation['date'][5:7]
+        date_operation_day = self.dict_operation['date'][8:10]
+        return f'{date_operation_day}.{date_operation_month}.{date_operation_year}'
