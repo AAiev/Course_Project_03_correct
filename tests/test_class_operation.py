01f67a02a44ef2de5d271a1efe094a5b248e1f60
Aleksandr Ievlev <a.a.ievlev@gmail.com>
2023-06-16 22:47:26 +0300

    add amount_operation add docs edit in PEP8

--- tests/test_class_operation.py
+++ tests/test_class_operation.py
@@ -36,12 +36,14 @@ def test_date_operation():
     assert test_operation.date_operation() == '08.12.2019'
     assert test_operation_2.date_operation() == '07.12.2019'
 
+
 def test_title_operation():
     """
     тест для функции title_operation - возвращает название операции
     """
     assert test_operation.title_operation() == 'Открытие вклада'
     assert test_operation_2.title_operation() == 'Перевод организации'
+
 
 def test_meaning_from():
     """
@@ -51,7 +53,18 @@ def test_meaning_from():
     assert test_operation_2.meaning_from() == 'Visa Classic 2842 87** **** 9012 -> '
     assert test_operation_3.meaning_from() == 'Счет **3655 -> '
 
+
 def test_meaning_to():
+    """
+    тест для функции meaning_to - возвращает поле куда
+    """
     assert test_operation.meaning_to() == 'Счет **5907'
     assert test_operation_2.meaning_to() == 'Счет **3655'
     assert test_operation_3.meaning_to() == 'Счет '
+
+
+def test_amount_operation():
+    """
+    тест для функции amount_operation - возвращает сумму и валюту операции
+    """
+    assert test_operation.amount_operation() == '41096.24 USD'
