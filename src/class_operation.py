3b7e8ddc0e81ec52878948f17ff1a454de1eecc1
Aleksandr Ievlev <a.a.ievlev@gmail.com>
2023-06-16 22:30:55 +0300

    add meaning_from

--- src/class_operation.py
+++ src/class_operation.py
@@ -20,3 +20,31 @@ class Operation:
         возвращает название операции
         """
         return self.dict_operation['description']
+
+    def meaning_from(self):
+        """
+        возвращает поле ОТКУДА в отформатированном варианте
+        """
+        if 'from' in self.dict_operation:
+            title_from = self.dict_operation['from']
+            digit_str = []
+            sum_digit_in_str = sum(1 for i in title_from if i.isdigit())
+            for i in title_from:
+                if i.isdigit():
+                    if len(digit_str) > 6 and (len(digit_str) < (sum_digit_in_str - 4 + digit_str.count(' '))):
+                        digit_str.append('*')
+                        if len(digit_str) % 4 == (0 + digit_str.count(' ')) and len(digit_str) > 0:
+                            digit_str.append(' ')
+                    else:
+                        digit_str.append(i)
+                        if len(digit_str) % 4 == (0 + digit_str.count(' ')) and 0 < len(digit_str) < sum_digit_in_str:
+                            digit_str.append(' ')
+
+            str_not_digit = ''.join([i for i in self.dict_operation['from'] if not i.isdigit()])
+            if 'счет' in title_from.lower():
+                return f"{str_not_digit}{''.join(digit_str[-7:]).replace(' ', '')} -> "
+            else:
+                return f"{str_not_digit}{''.join(digit_str[0:])} -> "
+
+        else:
+            return ""
